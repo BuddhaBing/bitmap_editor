834dc357d0cdc7913528e4fb567a3ab90b2185a4
Rob Brentnall <rob.brentnall@gmail.com>
2017-08-01 21:09:09 +0100

    first tests written for Bitmap and old code removed

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,30 +2,4 @@ require 'matrix'
 
 class Bitmap < Matrix
 
-  attr_reader :pixels
-
-  def []=(x, y, c)
-    @rows[y][x] = c
-  end
-
-  def fill_column(x, y1, y2, c)
-    y1.upto(y2) { |y| self[x,y] = c }
-  end
-
-  def fill_row(x1, x2, y, c)
-    x1.upto(x2) { |x| self[x,y] = c }
-  end
-
-  def pixels
-    @pixels ||= (1..rows).map{ (1..cols).map{'O'} }
-  end
-
-  private
-
-  attr_reader :rows, :cols, :error_strategy
-
-  def valid?
-    rows.between?(1, 250) && cols.between?(1, 250)
-  end
-
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -2,7 +2,7 @@ require 'bitmap'
 
 describe Bitmap do
 
-  subject { described_class.new(5,6) }
+  subject { described_class.new(5,6, 'O') }
 
   context '#build' do
 
@@ -10,7 +10,22 @@ describe Bitmap do
 
       it 'should raise an error if the given row size is less than 1' do
         expected_error = "Bitmap pixel width must be between 1 and 250"
-        expect{described_class.new(255,6)}.to raise_error expected_error
+        expect{described_class.new(-1, 1, 'O')}.to raise_error expected_error
+      end
+
+      it 'should raise an error if the given row size is greater than 250' do
+        expected_error = "Bitmap pixel width must be between 1 and 250"
+        expect{described_class.new(251, 1, 'O')}.to raise_error expected_error
+      end
+
+      it 'should raise an error if the given column size is less than 1' do
+        expected_error = "Bitmap pixel height must be between 1 and 250"
+        expect{described_class.new(1, -1, 'O')}.to raise_error expected_error
+      end
+
+      it 'should raise an error if the given column size is greater than 250' do
+        expected_error = "Bitmap pixel height must be between 1 and 250"
+        expect{described_class.new(1, 251, 'O')}.to raise_error expected_error
       end
 
     end
