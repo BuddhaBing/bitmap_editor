dc857cc87793cd332b5d103a031c31a065cf579a
Rob Brentnall <rob.brentnall@gmail.com>
2017-08-01 21:26:49 +0100

    implemented code for new instance of Bitmap and size validation - first tests passing

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,4 +2,27 @@ require 'matrix'
 
 class Bitmap < Matrix
 
+  MIN_SIZE = 1
+  MAX_SIZE = 250
+
+  def self.new(rows, cols)
+    build(rows, cols)
+  end
+
+  def self.build(rows, cols)
+    validate(rows, cols)
+    super
+  end
+
+  private
+
+  def self.validate(rows, cols)
+    raise "#{self} pixel width must be between #{MIN_SIZE} and #{MAX_SIZE}" if !valid?(rows)
+    raise "#{self} pixel height must be between #{MIN_SIZE} and #{MAX_SIZE}" if !valid?(cols)
+  end
+
+  def self.valid?(pixels)
+    pixels.between?(MIN_SIZE, MAX_SIZE)
+  end
+
 end
