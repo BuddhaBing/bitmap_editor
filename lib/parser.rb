bf27ffacbda0d3ba584d0a65da591e171e3d7174
Rob Brentnall <rob.brentnall@gmail.com>
2017-08-02 08:49:18 +0100

    commands hash created in parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,12 @@
 module Parser
 
+    COMMANDS = {
+        'I' => 'create',
+        'L' => 'colour_pixel',
+        'V' => 'colour_column',
+        'H' => 'colour_row',
+        'C' => 'clear',
+        'S' => 'show'
+    }
+
 end
