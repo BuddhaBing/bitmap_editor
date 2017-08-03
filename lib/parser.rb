70052f8fca28d2c5485a30f4fb7c9b9232797dbc
Rob Brentnall <rob.brentnall@gmail.com>
2017-08-03 11:23:25 +0100

    Parser.parse method correctly returns command

--- lib/parser.rb
+++ lib/parser.rb
@@ -9,4 +9,11 @@ module Parser
         'S' => 'show'
     }
 
+    def self.parse(line)
+      line = line.split(/\s+/)
+      command = line.shift.to_s.upcase
+      params = line.map { |el| el.match(/\A[-+]?\d+\z/) ? el.to_i - 1 : el }
+      return COMMANDS[command]
+    end
+
 end
